59addbe58a66c0099eedc0fcc700c429e5c8f0d3
gloamingblue <1992491680@qq.com>
2026-03-10 17:42:52 +0800

    add DQN

---
+++ DQN.py
@@ -0,0 +1,106 @@
+import random
+import gymnasium as gym
+import numpy as np
+import torch
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+from rl_utils import ReplayBuffer, train_off_policy_agent, moving_average
+
+
+class Qnet(torch.nn.Module):
+    """只有一层隐藏层的Q网络"""
+    def __init__(self, state_dim: int, hidden_dim: int, action_dim: int) -> None:
+        super(Qnet, self).__init__()
+        self.fc1 = torch.nn.Linear(state_dim, hidden_dim)
+        self.fc2 = torch.nn.Linear(hidden_dim, action_dim)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        return self.fc2(x)
+    
+
+class DQN:
+    """DQN算法"""
+    def __init__(
+            self, state_dim: int, hidden_dim: int, action_dim: int, 
+            learning_rate: float, gamma: float, epsilon: float, target_update: int, 
+            device: torch.device
+    ) -> None:
+        self.action_dim = action_dim
+        self.q_net = Qnet(state_dim, hidden_dim, self.action_dim).to(device)
+        # 目标网络
+        self.target_q_net = Qnet(state_dim, hidden_dim, self.action_dim).to(device)
+        # 使用Adam优化器
+        self.optimizer = torch.optim.AdamW(self.q_net.parameters(), lr=learning_rate)
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.target_update = target_update  # 目标网络更新频率
+        self.count = 0  # 记录更新次数
+        self.device = device
+        
+    def take_action(self, state):
+        if np.random.random() < self.epsilon:
+            action = np.random.randit(self.action_dim)
+        else:
+            state = torch.tensor([state], dtype=torch.float).to(self.device)
+            action = self.q_net(state).argmax().item()
+        return action
+    
+    def update(self, transition_dict) -> None:
+        states = torch.tensor(transition_dict['states'], dtype=torch.float).to(self.device)
+        actions = torch.tensor(transition_dict['actions']).view(-1, 1).to(self.device)
+        rewards = torch.tensor(transition_dict['rewards'], dtype=torch.float).to(self.device)
+        next_states = torch.tensor(transition_dict['next_states'], dtype=torch.float).to(self.device)
+        dones = torch.tensor(transition_dict['dones'], dtype=torch.float).view(-1, 1).to(self.device)
+
+        q_values = self.q_net(states).gather(1, actions)
+        max_next_q_values = self.target_q_net(next_states).max(1)[0].view(-1, 1)  # 下个状态的最大Q值
+        q_targets = rewards + self.gamma * max_next_q_values * (1 - dones)
+
+        dqn_loss = torch.mean(F.mse_loss(q_values, q_targets))
+        self.optimizer.zero_grad()
+        dqn_loss.backward()
+        self.optimizer.step()
+
+        if self.count % self.target_update == 0:
+            self.target_q_net.load_state_dict(self.q_net.state_dict())
+        self.count += 1
+
+
+if __name__ == '__main__':
+    lr = 2e-3
+    num_episodes = 500
+    hidden_dim = 128
+    gamma = 0.98
+    epsilon = 0.01
+    target_update = 10
+    buffer_size = 10000
+    minimal_size = 500
+    batch_size = 64
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+    env_name = 'CartPole-v0'
+    env = gym.make(env_name)
+    random.seed(42)
+    np.random.seed(42)
+    # env.seed(42)
+    torch.manual_seed(42)
+    replay_buffer = ReplayBuffer(buffer_size)
+    state_dim = env.observation_space.shape[0]
+    action_dim = env.action_space.n
+    agent = DQN(state_dim, hidden_dim, action_dim, lr, gamma, epsilon, target_update, device)
+
+    return_list = train_off_policy_agent(env, agent, num_episodes, replay_buffer, minimal_size, batch_size)
+    episodes_list = list(range(len(return_list)))
+    plt.plot(episodes_list, return_list)
+    plt.xlabel('Episodes')
+    plt.ylabel('Returns')
+    plt.title('DQN on {}'.format(env_name))
+    plt.show()
+
+    mv_return = moving_average(return_list, 9)
+    plt.plot(episodes_list, mv_return)
+    plt.xlabel('Episodes')
+    plt.ylabel('Returns')
+    plt.title('DQN on {}'.format(env_name))
+    plt.show()
